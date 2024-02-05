89f85b63ba9efbecb41b3893f69e7bc733b350d6
DracoStorm <119288973+DracoStorm@users.noreply.github.com>
2024-02-04 23:02:26 -0600

    adding a legend for the data

--- simulation/plot_animation.py
+++ simulation/plot_animation.py
@@ -12,15 +12,15 @@ def update_plot(frame, p1, p2, line1, line2, scatter1, scatter2):
 
 def plot_animation(t, p1, p2, N):
     fig, ax = plt.subplots()
-    line1, = ax.plot(p1[0], p2[0], 'k')
-    line2, = ax.plot(p1[0], p2[0], 'b')
-    scatter1 = ax.scatter(p1[0], p2[0], c='k', marker='o')
-    scatter2 = ax.scatter(p1[0], p2[0], c='b', marker='o')
+    line1, = ax.plot(p1[0], p2[0], 'k', label='trajectory_planet_1')
+    line2, = ax.plot(p1[0], p2[0], 'b', label='trajectory_planet_2')
+    scatter1 = ax.scatter(p1[0], p2[0], c='k', marker='o', label='planet_1')
+    scatter2 = ax.scatter(p1[0], p2[0], c='b', marker='o', label='planet_2')
 
     ax.grid(True)
     ax.set(xlim=[0.1, 0.4], ylim=[0.1, 0.2],
            xlabel='Distance [x]', ylabel='Distance [y]')
-
+    ax.legend()
     ani = FuncAnimation(fig=fig, func=update_plot, frames=N, fargs=(
         p1, p2, line1, line2, scatter1, scatter2), interval=30, blit=True)
 
