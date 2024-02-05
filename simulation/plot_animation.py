690c65cecc3fb6a98972d86bda5494e89c8cd395
DracoStorm <119288973+DracoStorm@users.noreply.github.com>
2024-02-04 22:19:41 -0600

    rewrite the module to use FuncAnimation instead of generate plot windows

---
+++ simulation/plot_animation.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+
+def update_plot(frame, p1, p2, line1, line2, scatter1, scatter2):
+    line1.set_data(p1[:frame, 0], p1[:frame, 1])
+    line2.set_data(p2[:frame, 0], p2[:frame, 1])
+    scatter1.set_offsets(p1[frame, :])
+    scatter2.set_offsets(p2[frame, :])
+    return line1, line2, scatter1, scatter2
+
+
+def plot_animation(t, p1, p2, N):
+    fig, ax = plt.subplots()
+    line1, = ax.plot(p1[0], p2[0], 'k')
+    line2, = ax.plot(p1[0], p2[0], 'b')
+    scatter1 = ax.scatter(p1[0], p2[0], c='k', marker='o')
+    scatter2 = ax.scatter(p1[0], p2[0], c='b', marker='o')
+
+    ax.grid(True)
+    ax.set(xlim=[0.1, 0.4], ylim=[0.1, 0.2],
+           xlabel='Distance [x]', ylabel='Distance [y]')
+
+    ani = FuncAnimation(fig=fig, func=update_plot, frames=N, fargs=(
+        p1, p2, line1, line2, scatter1, scatter2), interval=30, blit=True)
+
+    plt.show()
