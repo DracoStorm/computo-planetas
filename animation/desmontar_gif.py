0096756806966ae4507c5ea374096468052f4a2c
DracoStorm <119288973+DracoStorm@users.noreply.github.com>
2024-02-04 22:41:35 -0600

    updating variables names to english (shorter), updating the docs in consequence

--- animation/desmontar_gif.py
+++ animation/desmontar_gif.py
@@ -3,47 +3,47 @@ from PIL import Image, ImageSequence
 
 
 # Ruta de la carpeta gif y fotogramas desde la raiz
-RUTA_GIF = r"animation/gifs/"
-RUTA_FOTOGRAMAS = r"animation/fotogramas/"
+PATH_GIF = r"animation/gifs/"
+PATH_FRAME = r"animation/frames/"
 
 
-def desmontar_gif(archivo_gif):
+def desmontar_gif(file):
     """
-        Desmonta el gif en cada frame que lo contiene y lo deposita en la carpta RUTA_FOTOGRAMAS.
+        Desmonta el gif en cada frame que lo contiene y lo deposita en la carpta PATH_FRAMES.
 
             Parameters
             ----------
-                arhivo_gif : string
-                    Nombre de un archivo dentro de RUTA_GIF.
+                file : string
+                    Nombre de un archivo .gif dentro de PATH_GIF.
 
             Exceptions
             ----------
-                Si la ruta del archivo o destino no es encontrada.
+                Si el archivo no existe.
     """
 
-    # Verfifica si el directorio "fotogramas" existe, si no lo crea.
-    if (not os.path.isdir(RUTA_FOTOGRAMAS)):
-        os.mkdir(RUTA_FOTOGRAMAS)
+    # Verfifica si el directorio "./animation/frames/" existe, si no lo crea.
+    if (not os.path.isdir(PATH_FRAME)):
+        os.mkdir(PATH_FRAME)
 
-    # Verfifica si el directorio "animation/fotogramas/archivo_gif" existe, si no lo crea.
+    # Verfifica si el directorio "./animation/frames/file/" existe, si no lo crea.
     # De esta manera existe una carpeta por cada gif.
-    RUTA_FOTOGRAMAS_GIF = RUTA_FOTOGRAMAS + archivo_gif + r"/"
-    if (not os.path.isdir(RUTA_FOTOGRAMAS_GIF)):
-        os.mkdir(RUTA_FOTOGRAMAS_GIF)
+    PATH_FRAME_GIF = PATH_FRAME + file + r"/"
+    if (not os.path.isdir(PATH_FRAME_GIF)):
+        os.mkdir(PATH_FRAME_GIF)
 
-    gif = Image.open(RUTA_GIF + archivo_gif + ".gif")
+    gif = Image.open(PATH_GIF + file + ".gif")
 
     try:
-        # Extrae todos los fotogramas del GIF
-        fotogramas = [f.convert("RGBA") for f in ImageSequence.Iterator(gif)]
+        # Extrae todos los frames del file
+        frames = [f.convert("RGBA") for f in ImageSequence.Iterator(gif)]
 
-        # Guarda cada fotograma como una imagen separada
-        for i, fotograma in enumerate(fotogramas):
-            archivo_png = RUTA_FOTOGRAMAS_GIF + f"fotograma_{i}.png"
-            fotograma.save(archivo_png, "PNG")
+        # Guarda cada frame como una imagen separada
+        for i, frame in enumerate(frames):
+            name_frame = PATH_FRAME_GIF + f"frame_{i}.png"
+            frame.save(name_frame, "PNG")
 
         print(
-            f"Se han desmontado correctamente {len(fotogramas)} fotogramas del gif {archivo_gif} en la carpeta {RUTA_FOTOGRAMAS_GIF}.")
+            f"Se han desmontado correctamente {len(frames)} frames del gif {file} en la carpeta {PATH_FRAME_GIF}.")
     except Exception as e:
         print(f"Error al desmontar el GIF: {e}")
 
