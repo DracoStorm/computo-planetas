c1faacaa72716e94ad4b8f47ea23ad23e2ba67cb
DracoStorm <119288973+DracoStorm@users.noreply.github.com>
2024-03-18 00:00:46 -0600

    cambiando de dict a array

--- network/constants.py
+++ network/constants.py
@@ -3,8 +3,9 @@ FILE_IDENTIFIER = b'FILE'
 MSG_IDENTIFIER = b'MSG'
 
 # Diccionario de ips según los componentes
-IPS = {
+IPS = [
     '172.26.167.179',  # ip-Toño
     '172.31.6.164',  # ip-Draco
-    '172.31.12.11'  # ip-Draco1
-}
+    '172.31.12.11',  # ip-Draco1
+    '192.168.100.21'
+]
