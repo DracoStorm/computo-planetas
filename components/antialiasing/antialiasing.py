b0d26946a8eab6118020b92db36b4ca66c0573fb
Antonio Juárez <jose.coyotzi@alumno.buap.mx>
2024-04-04 13:41:15 -0600

    envio y recivo de bites y suavisado

--- components/antialiasing/antialiasing.py
+++ components/antialiasing/antialiasing.py
@@ -1,13 +1,30 @@
 from PIL import Image, ImageFilter
-import os
 
 
-def antialising(file: str) -> Image:
+def antialiasing(file: str) -> Image:
     # Abrir la imagen original
-    file = Image.open(file)
+    imagen_original = Image.open(file)
 
     # Aplicar el filtro gaussiano
-    alias_file = file.filter(ImageFilter.GaussianBlur(
+    imagen_suavizada = imagen_original.filter(ImageFilter.GaussianBlur(
         radius=2))  # Puedes ajustar el radio según tus necesidades
 
-    return alias_file
+    return imagen_suavizada
+
+
+def main():
+    # Ruta de la imagen que deseas suavizar
+    # Cambia esto a la ruta de tu imagen
+    ruta_imagen = "components/antialiasing/"
+
+    # Aplicar antialiasing a la imagen
+    imagen_suavizada = antialiasing(ruta_imagen)
+
+    # Guardar la imagen suavizada
+    nombre_imagen_suavizada = "mi_imagen_suavizada.jpg"
+    imagen_suavizada.save(nombre_imagen_suavizada)
+    print(f"Imagen suavizada guardada como {nombre_imagen_suavizada}")
+
+
+if __name__ == "__main__":
+    main()
--- network/functions.py
+++ network/functions.py
@@ -3,7 +3,7 @@ from socket import socket
 from constants import *
 
 
-def recive_file(client_socket: socket, initial_data: bytes) -> None:
+def receive_file(client_socket: socket, initial_data: bytes) -> None:
     try:
         file_info = initial_data[len(FILE_IDENTIFIER):].decode(
             'utf-8', errors='replace').split('@')
@@ -22,118 +22,99 @@ def recive_file(client_socket: socket, initial_data: bytes) -> None:
                 received_data += len(file_data)
 
         print("File received successfully.")
+        print(f"File size: {os.path.getsize(file_name)} bytes")
     except Exception as e:
         print(f"Error during file transfer: {e}")
 
 
-def recive_file(client_socket: socket, initial_data: bytes) -> tuple[str, int, bytes]:
+def receive_file_info(client_socket: socket, initial_data: bytes) -> tuple[str, int, bytes]:
     try:
-        file_info: str = initial_data[len(FILE_IDENTIFIER):].decode(
+        file_info = initial_data[len(FILE_IDENTIFIER):].decode(
             'utf-8', errors='replace').split('@')
+        file_name = file_info[0]
+        file_size = int(file_info[1])
 
-        # Initialize an empty bytearray to store the file data
-        file_data: bytearray = bytearray()
-
+        file_data = bytearray()
         while True:
-            data_chunk: bytes = client_socket.recv(1024)
+            data_chunk = client_socket.recv(1024)
             if not data_chunk:
-                break  # No more data, end the loop
+                break
             file_data.extend(data_chunk)
     except Exception as e:
         print(f"Error during file transfer: {e}")
     else:
-        # return the file name, file size, file in bytes
-        return file_info[0], int(file_info[1]), bytes(file_data)
+        return file_name, file_size, bytes(file_data)
 
 
-def recive_message(initial_data: bytes) -> str:
+def receive_message(initial_data: bytes) -> str:
     try:
         message = str(initial_data[len(MSG_IDENTIFIER):].decode(
             'utf-8', errors='replace'))
+        print(f"Message received. Size: {len(message.encode('utf-8'))} bytes")
     except Exception as e:
         print(f"Error during message transfer: {e}")
     else:
         return message
 
 
-def send_file(client_socket, file_path):
-    # Obtener el nombre y tamaño del archivo
-    file_name = os.path.basename(file_path)
-    file_size = os.path.getsize(file_path)
-
+def send_file(client_socket: socket, file_path: str) -> None:
     try:
-        # Send file information
-        file_info = f"{file_name}@{file_size}@"
-        client_socket.sendall(FILE_IDENTIFIER +
-                              file_info.encode('utf-8'))
+        file_name = os.path.basename(file_path)
+        file_size = os.path.getsize(file_path)
 
-        # Send the file data
+        file_info = f"{file_name}@{file_size}@"
+        client_socket.sendall(FILE_IDENTIFIER + file_info.encode('utf-8'))
+
         with open(file_path, 'rb') as file:
             data = file.read(1024)
             while data:
                 client_socket.sendall(data)
                 data = file.read(1024)
-    except Exception as e:
-        print(f"Error during file transfer: {e}")
 
-
-def send_file(client_socket: socket, file_info: tuple[str, int], file: bytes):
-    try:
-        # Send file information
-        file_info = f"{file_info[0]}@{str(file_info[1])}@"
-        client_socket.sendall(FILE_IDENTIFIER + file_info.encode('utf-8'))
-    except Exception as e:
-        print(f"Error during file transfer: {e}")
-    try:
-        client_socket.sendall(file)
+        print(f"File sent. Size: {file_size} bytes")
     except Exception as e:
         print(f"Error during file transfer: {e}")
 
 
 def send_message(client_socket: socket, message: str) -> None:
     try:
-        # Enviar el mensaje al servidor
         client_socket.sendall(MSG_IDENTIFIER + message.encode('utf-8'))
+        print(f"Message sent. Size: {len(message.encode('utf-8'))} bytes")
     except Exception as e:
-        print(f"Error durante el envío del mensaje: {e}")
+        print(f"Error during message transfer: {e}")
 
 
 def send_error(client_socket: socket, error: str) -> None:
     try:
-        # Enviar el mensaje al servidor
         client_socket.sendall(ERR_IDENTIFIER + error.encode('utf-8'))
     except Exception as e:
-        print(f"Error durante el envío del mensaje: {e}")
+        print(f"Error during error transfer: {e}")
 
 
 def send_shutdown(client_socket: socket) -> None:
     try:
-        # Enviar el mensaje al servidor
         client_socket.sendall(SHUTDOWN_IDENTIFIER)
     except Exception as e:
-        print(f"Error durante el envío del mensaje: {e}")
+        print(f"Error during shutdown transfer: {e}")
 
 
-def handle_client(client_socket):
+def handle_client(client_socket: socket) -> None:
     try:
         while True:
             data = client_socket.recv(1024)
 
             if not data:
-                break  # No more data, close the connection
+                break
 
             if data.startswith(FILE_IDENTIFIER):
-                print("Message")
-                recive_file(client_socket, data)
-
+                print("File")
+                receive_file(client_socket, data)
             elif data.startswith(MSG_IDENTIFIER):
-                recive_message(data)
+                print("Message")
+                receive_message(data)
             else:
                 print("Unknown data type.")
-
     except Exception as e:
         print(f"Error during data transfer: {e}")
-
     finally:
-        # Close the client connection
         client_socket.close()
