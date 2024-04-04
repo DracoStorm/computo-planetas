99d343845f968c6a08265e8141d326fbb057f222
Antonio Juárez <jose.coyotzi@alumno.buap.mx>
2024-04-04 16:37:39 -0600

    separacion de suavizado

--- components/antialiasing/antialiasing.py
+++ components/antialiasing/antialiasing.py
@@ -1,29 +1,61 @@
 from PIL import Image, ImageFilter
 
 
-def antialiasing(file: str) -> Image:
-    # Abrir la imagen original
-    imagen_original = Image.open(file)
+def cargar_imagen(ruta: str) -> Image:
+    """
+    Carga una imagen desde la ruta especificada.
 
-    # Aplicar el filtro gaussiano
-    imagen_suavizada = imagen_original.filter(ImageFilter.GaussianBlur(
-        radius=2))  # Puedes ajustar el radio según tus necesidades
+    Args:
+    - ruta (str): Ruta de la imagen a cargar.
 
+    Returns:
+    - Image: Objeto de imagen PIL.
+    """
+    imagen = Image.open(ruta)
+    return imagen
+
+
+def aplicar_antialiasing(imagen: Image, radio: int = 2) -> Image:
+    """
+    Aplica antialiasing a la imagen utilizando un filtro gaussiano.
+
+    Args:
+    - imagen (Image): Objeto de imagen PIL.
+    - radio (int): Radio del filtro gaussiano. Valor por defecto: 2.
+
+    Returns:
+    - Image: Imagen suavizada.
+    """
+    imagen_suavizada = imagen.filter(ImageFilter.GaussianBlur(radius=radio))
     return imagen_suavizada
+
+
+def guardar_imagen(imagen: Image, nombre: str):
+    """
+    Guarda la imagen en disco con el nombre especificado.
+
+    Args:
+    - imagen (Image): Objeto de imagen PIL.
+    - nombre (str): Nombre de archivo para guardar la imagen.
+    """
+    imagen.save(nombre)
+    print(f"Imagen guardada como {nombre}")
 
 
 def main():
     # Ruta de la imagen que deseas suavizar
     # Cambia esto a la ruta de tu imagen
-    ruta_imagen = "components/antialiasing/"
+    ruta_imagen = "components/antialiasing/zelda.jpg"
+
+    # Cargar la imagen
+    imagen_original = cargar_imagen(ruta_imagen)
 
     # Aplicar antialiasing a la imagen
-    imagen_suavizada = antialiasing(ruta_imagen)
+    imagen_suavizada = aplicar_antialiasing(imagen_original)
 
     # Guardar la imagen suavizada
     nombre_imagen_suavizada = "mi_imagen_suavizada.jpg"
-    imagen_suavizada.save(nombre_imagen_suavizada)
-    print(f"Imagen suavizada guardada como {nombre_imagen_suavizada}")
+    guardar_imagen(imagen_suavizada, nombre_imagen_suavizada)
 
 
 if __name__ == "__main__":
